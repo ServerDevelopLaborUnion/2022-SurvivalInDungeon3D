b87fc2a0296541ac1f5c3f6ab0064ad6fc4ee625
KBluePurple <42506968+BlWon@users.noreply.github.com>
2022-09-27 23:50:30 +0900

    약간의 업데이트

--- ProtoBuf/make.bat
+++ ProtoBuf/make.bat
@@ -1,3 +1,3 @@
-.\\protoc.exe --csharp_out="../Assets\01.Scripts\08. Protobuf" Server.proto
-.\\protoc.exe --csharp_out="../Assets\01.Scripts\08. Protobuf" Client.proto
-.\\protoc.exe --csharp_out="../Assets\01.Scripts\08. Protobuf" Type.proto
+.\\protoc.exe --csharp_out="./Generated" Server.proto
+.\\protoc.exe --csharp_out="./Generated" Client.proto
+.\\protoc.exe --csharp_out="./Generated" Type.proto
